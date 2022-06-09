150e508dd1e064a09595301856e088c6c471277d
Chandra Sekhar Sahu <74488706+cssahu3988@users.noreply.github.com>
2022-06-09 13:02:01 +0530

    commit-1

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Admin Panel',
